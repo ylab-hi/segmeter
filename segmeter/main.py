fbe28351cca305b4ee38138a7a479e4d1070f124
riasc <richard.schaefer@zoho.com>
2024-11-29 22:02:12 -0600

    data cleanup

--- segmeter/main.py
+++ segmeter/main.py
@@ -2,21 +2,26 @@ from pathlib import Path
 import argparse
 import os
 import time
+import logging
 
 from simulator import SimBase
+from benchmark import BenchBase
+import utility
 
 def main():
     options = parse_arguments()
-    print(options)
+    logfile = Path(options.outdir) / 'segmeter.log'
+    logger = utility.setup_logger(logfile, options.loglevel, "")
 
     output = Path(options.outdir) / options.modus
     output.mkdir(parents=True, exist_ok=True)
+    intvlnums = det_intvlnums(options.intvlnums)
 
     if options.modus == "sim":
-        bed = SimBase(options)
+        bed = SimBase(options, intvlnums)
         bed.format.simulate()
-
-
+    elif options.modus == "bench":
+        bench = BenchBase(options, intvlnums, logger)
 
     # look up the files to benchmark
     # fh = open(options.list, "r")
@@ -32,7 +37,10 @@ def parse_arguments():
     parser.add_argument("modus", type=str, help="modus in benchmarking", choices=["sim", "bench"])
     parser.add_argument("-f", "--format", type=str, help="format of the files to benchmark", choices=["BED"], default="BED")
     parser.add_argument("-n", "--intvlnums", type=str, help="number of intervals to simulate", default="10")
-    parser.add_argument("-o", "--outdir", type=str, help="output folder for the benchmark results", required=True)
+    parser.add_argument("-o", "--outdir", type=str, help="output folder for the benchmark/simulation results. ", required=True)
+    parser.add_argument("-t", "--tool", type=str, help="tool to benchmark", choices=["tabix", "bedtools"])
+    parser.add_argument("-l", "--loglevel", type=str, help="Log level", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], default="INFO")
+    parser.add_argument("-d", "--datatype", type=str, help="datatype of the intervals", choices=["simple", "complex", "custom"], default="simple")
 
     args = parser.parse_args()
 
@@ -40,6 +48,16 @@ def parse_arguments():
     script_dir = Path(__file__).parent.resolve()
 
     return args
+
+def det_intvlnums(intvlnums):
+    intnums = {}
+    nummap = {'K': 1000, 'M': 1000000}
+    for num in intvlnums.split(","):
+        if num[-1] in nummap:
+            intnums[num] = int(num[:-1]) * nummap[num[-1]]
+        else:
+            intnums[num] = int(num)
+    return intnums
 
 
 # def tabix_call(inputfile, outputdir):
