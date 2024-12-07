864fedc4b4e8629fd45a46bc02dc08deb12bd5b5
riasc <richard.schaefer@zoho.com>
2024-12-07 00:12:08 -0600

    added option for maximum chromosome length

--- segmeter/main.py
+++ segmeter/main.py
@@ -35,6 +35,7 @@ def parse_arguments():
     parser.add_argument("modus", type=str, help="modus in benchmarking", choices=["sim", "bench"])
     parser.add_argument("-f", "--format", type=str, help="format of the files to benchmark", choices=["BED"], default="BED")
     parser.add_argument("-n", "--intvlnums", type=str, help="number of intervals to simulate", default="10")
+    parser.add_argument("-m", "--max_chromlen", type=int, help="maximum length of the simulated chromosomes", default=1000000000)
     parser.add_argument("-o", "--outdir", type=str, help="output folder for the benchmark/simulation results. ", required=True)
     parser.add_argument("-t", "--tool", type=str, help="tool to benchmark", choices=["tabix", "bedtools"])
     parser.add_argument("-g", "--gapsize", type=str, help="random size of the gaps (min and max) between the intervals", default="100-5000")
